ddf82f25e524a2c8f341b3a51e836d792e507edb
MelissaCFox <melissa.caitlin.fox@gmail.com>
2021-10-19 14:03:03 -0500

    initial commit

---
+++ scripts/Orders.js
@@ -0,0 +1,66 @@
+import { addCustomOrder, getOrders, getMetals, getStyles, getSizes } from "./database.js"
+
+
+const metals = getMetals()
+const foundMetal = metals.find(
+    (metal) => {
+        return metal.id === order.metalId
+    }
+)
+
+const stylesArray = getStyles()
+const foundStyle = stylesArray.find(
+    (style) => {
+        return style.id === order.styleId
+    }
+)
+
+const sizesArray = getSizes()
+const foundSize = sizesArray.find(
+    (size) => {
+        return size.id === order.sizeId
+    }
+)
+
+
+const totalCost = foundMetal.price + foundStyle.price + foundSize.price
+
+const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+})
+
+
+
+const buildOrderListItem = (order) => {
+    return `<li>
+        Order #${order.id} costs ${costString}
+        </li>`
+}
+
+export const Orders = () => {
+    /*
+    Can you explain why the state variable has to be inside
+    the component function for Orders, but not the others?
+    */
+    const orders = getOrders()
+
+    let html = "<ul>"
+
+    const listItems = orders.map(buildOrderListItem)
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const buttonClicked = clickEvent.target
+        if (buttonClicked.id === "orderButton") {
+            addCustomOrder()
+        }
+    }
+)
